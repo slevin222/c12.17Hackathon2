969bda479a9877c4201374bbe5624d88c948af7f
Justin Morinaka <justin_morinaka@yahoo.com>
2018-01-16 15:49:19 -0800

    fixed current location on google map"
    git branch

--- script.js
+++ script.js
@@ -76,22 +76,50 @@ function Display() {
 }
 
 
-var map;
-var infowindow;
+// var map;
+// var infowindow;
 
 //needs to call function initMap because
 
 function initMap() {
-    var current = {lat: 33.6441211395679, lng: -117.743128531307};
 
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15,
-        center: current
-    });
-    var marker = new google.maps.Marker({
-        position: current,
-        map: map
-    });
+
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+
+
+            };
+            console.log(pos)
+
+            var current = {lat: pos.lat, lng: pos.lng};
+            // var current = navigator.geolocation;
+
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 15,
+                center: current
+            });
+            var marker = new google.maps.Marker({
+                position: current,
+                map: map
+            });
+
+            // var infoWindow = new google.maps.InfoWindow;
+            //
+            // infoWindow.setPosition(pos);
+            // infoWindow.setContent('Location found.');
+            // infoWindow.open(map);
+            map.setCenter(pos);
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
 
 }
 
