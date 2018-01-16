8897d8292f480d47a93924964db9c6ed54b8596b
Shawn Levin <shawnlevin22@gmail.com>
2018-01-16 13:48:52 -0800

    recevied movie pics

--- script.js
+++ script.js
@@ -128,26 +128,30 @@ function createMarker(place) {
 
 }
  function movieDataFrontPage() {
-    let ajaxConfig = {
-        data: {
-            location:"33.6441211395679,-117.743128531307",
-            limit:10,
-            countries:'US',
-            fields: 'title,scene_images,synopsis,trailers'
-        },
-        type: 'GET',
-        url: 'https://api.internationalshowtimes.com/v4/movies/',
-        headers:{
-            'X-API-Key': 'UITTMomjJcICW40XNigMoGaaCSykTcYd'
-        },
-        success: function (result) {
-            if (result["success"]) {
-                console.log("Results: "+ result);
+     let ajaxConfig = {
+         data: {
+             location: "33.6441211395679,-117.743128531307",
+             limit: 10,
+             countries: 'US',
+             fields: 'title,scene_images,synopsis,trailers'
+         },
+         type: 'GET',
+         url: 'https://api.internationalshowtimes.com/v4/movies/',
+         headers: {
+             'X-API-Key': 'UITTMomjJcICW40XNigMoGaaCSykTcYd'
+         },
+         success: function (result) {
+             if (result["success"]) {
+                 console.log("Results: " + result);
 
-                for( let movieData = 0 ; movieData < result.movies.length; movieData++){
-                    $('movie'+movieData).attr(result.movies[movieData]);
+                 for (let movieData = 0; movieData < result.movies.length; movieData++) {
+                     $('movie' + movieData).attr(result.movies[movieData]);
 
-}
+                 }
+             }
+         }
+     }
+ }
 //
 // var map;
 // var infowindow;
@@ -219,7 +223,7 @@ function Movie(){
                         currentMovie[0].movie = result.movies[movieDataIndex];
                         currentMovie.css({
                             'background-image': "url('"+currentMovie[0].movie.poster_image+"')",
-                            'background-size': 'cover'
+                            // 'background-size': 'cover'
                             }
                         );
 
