e8cdea449f3322273e60aaa9076a5db6c9f5c6a6
HyeManMoon <hyemanmoon@gmail.com>
2018-01-16 12:07:37 -0800

    cleaned up style

--- script.js
+++ script.js
@@ -1,50 +1,43 @@
 $(document).ready(initializeApp);
 
 function initializeApp() {
-    display = new Display();
-    display.init();
+	var display = new Display();
+	display.init();
 }
 
-
 function Display() {
-    this.init = function () {
-        this.display();
-    };
-
-    this.display = function () {
-        var container = $("container");
-        for (var i = 1; i < 8; i++) {
-            var movieRow = $('<div>', {
-                class: 'movie'+i
-            });
-            container.append(movieRow);
-        }
-
-        for (var x = 1; x < 8; x++) {
-            var foodRow = $('<div>', {
-                class: 'foodType'+x
-            });
-            container.append(foodRow);
-        }
-
-        var movieInfo =  $('<div>', {
-            class: 'movieInfo'
-        });
-        container.append(movieInfo);
-
-        var displayMap =  $('<div>', {
-            class: 'displayMap'
-        });
-        container.append(displayMap);
-
-        var foodInfo =  $('<div>', {
-            class: 'foodInfo'
-        });
-        container.append(foodInfo);
-
-        var title = $('<div>', {
-            class: 'footer'
-        });
-        container.append(title);
-    }
+	this.init = function() {
+		this.display();
+	};
+	this.display = function() {
+		var container = $("#container");
+		for (var i = 1; i < 8; i++) {
+			var movieRow = $('<div>', {
+				class: 'movie' + i
+			});
+			container.append(movieRow);
+		}
+		for (var x = 1; x < 8; x++) {
+			var foodRow = $('<div>', {
+				class: 'foodType' + x
+			});
+			container.append(foodRow);
+		}
+		var movieInfo = $('<div>', {
+			class: 'movieInfo'
+		});
+		container.append(movieInfo);
+		var displayMap = $('<div>', {
+			class: 'displayMap'
+		});
+		container.append(displayMap);
+		var foodInfo = $('<div>', {
+			class: 'foodInfo'
+		});
+		container.append(foodInfo);
+		var title = $('<div>', {
+			class: 'footer'
+		});
+		container.append(title);
+	}
 }
